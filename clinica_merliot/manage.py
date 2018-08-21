e380ed159a7e3013e4f6743846370a01db04a8ab
Carlos Cativo <cativo23.kt@gmail.com>
2018-08-20 23:14:44 -0500

    Arreglo

--- clinica_merliot/manage.py
+++ clinica_merliot/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Sistema_Informatico_Gestion_Odontologica_Clinica_Dental_Merliot.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clinica_merliot.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
